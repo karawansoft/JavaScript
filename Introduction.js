4468209d149692f8fc8a4115d38ba8c5c859be80
simohammed <simohammed.akhart@gmail.com>
2023-02-17 13:17:05 +0100

    commit 1

---
+++ Introduction.js
@@ -0,0 +1,16 @@
+// Les commentaires :
+let i = 6
+for (let i=0;i<3;i++)
+{
+    console.log("ligne "+ i) // message affichage
+}
+console.log("resultat " + i)
+const stag = {num : 1 , nom : "ali"}
+console.log(stag.num +  " " + stag.nom)
+stag.num = 2
+stag.nom = "bachir"
+console.log(stag.num +  " " + stag.nom)
+alert("bjr")
+
+
+
